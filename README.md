198bf6c76e68325d5a5c1404d3ff424038c0764c
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-01 11:48:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Library Pair Programming
+This is a project made by Ryan Carson and Alex Malcolm while attending We Can Code IT.
---
+++ src/main/java/org/wecancodeit/librarypairprogramming/LibraryPairProgrammingApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.librarypairprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPairProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPairProgrammingApplication.class, args);
+	}
+}
